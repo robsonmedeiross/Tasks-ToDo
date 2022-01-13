5ba27e548af22814ad2c30c8a9675da0ece4d691
robson.medeiros <robson.medeiros@opah.com.br>
2022-01-12 22:53:13 -0300

    adicionando beaultiful-DnD no projeto - funcional com bug

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-
+import { useState } from 'react';
+import { DragDropContext, Droppable, Draggable, DropResult, } from 'react-beautiful-dnd';
 import '../styles/tasklist.scss'
-
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 
 interface Task {
@@ -9,10 +8,28 @@ interface Task {
   title: string;
   isComplete: boolean;
 }
+const finalSpaceCharacters = [
+  {
+    id: 'gary',
+    name: 'Gary Goodspeed',
+    thumb: '/images/gary.png'
+  },
+  {
+    id: 'cato',
+    name: 'Little Cato',
+    thumb: '/images/cato.png'
+  },
+  {
+    id: 'kvn',
+    name: 'KVN',
+    thumb: '/images/kvn.png'
+  },
+]
 
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
@@ -55,6 +72,18 @@ export function TaskList() {
     }
   }
 
+  function handleOnDragEnd(result: DropResult):void {
+    if (!result.destination) return;
+
+    const items = Array.from(tasks);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    console.log("itens "+items)
+    //updateCharacters(items);
+    console.log("characters "+ tasks)
+    setTasks(items)
+  }
+
   return (
     <section className="task-list container">
       <header>
@@ -74,28 +103,38 @@ export function TaskList() {
       </header>
 
       <main>
-        <ul>
-          {tasks.map(task => (
-            <li key={task.id}>
-              <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
-                <label className="checkbox-container">
-                  <input 
-                    type="checkbox"
-                    readOnly
-                    checked={task.isComplete}
-                    onClick={() => handleToggleTaskCompletion(task.id)}
-                  />
-                  <span className="checkmark"></span>
-                </label>
-                <p>{task.title}</p>
-              </div>
-              <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
-              </button>
-            </li>
-          ))}
-          
-        </ul>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="characters">
+            {(provided) => (
+              <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
+                {tasks.map((task, index) => (
+                  <Draggable key={task.id} draggableId={task.title} index={index}>
+                    {(provided) => (
+                      <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                        <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
+                          <label className="checkbox-container">
+                            <input 
+                              type="checkbox"
+                              readOnly
+                              checked={task.isComplete}
+                              onClick={() => handleToggleTaskCompletion(task.id)}
+                            />
+                            <span className="checkmark"></span>
+                          </label>
+                          <p>{task.title}</p>
+                        </div>
+                        <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
+                          <FiTrash size={16}/>
+                        </button>
+                      </li>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
       </main>
     </section>
   )
